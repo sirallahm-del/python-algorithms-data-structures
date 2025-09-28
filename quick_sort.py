1a96e6c8f79e5ee31c794445bf9afcbf8642d16c
sirallahm-del <sirallahm@gmail.com>
2025-09-28 19:24:00 +0100

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -10,6 +10,32 @@ print("Notre pivot est:", pivot)
 # On crée deux listes vides
 plus_petits = []
 plus_grands = []
+# Quick Sort étape par étape
+# Auteur: Sirallah
 
-print("Listes créées - plus_petits:", plus_petits)
-print("Listes créées - plus_grands:", plus_grands)
+# Notre liste à trier
+liste = [5, 2, 8, 1, 9]
+print("Liste de départ:", liste)
+
+# On choisit le premier élément comme pivot
+pivot = liste[0]
+print("Notre pivot est:", pivot)
+
+# On crée deux listes vides
+plus_petits = []
+plus_grands = []
+
+# On regarde chaque élément après le premier
+for i in range(1, len(liste)):
+    element = liste[i]
+    print("On regarde l'élément:", element)
+    
+    if element < pivot:
+        plus_petits.append(element)
+        print(element, "va dans plus_petits")
+    else:
+        plus_grands.append(element)
+        print(element, "va dans plus_grands")
+
+print("Plus petits:", plus_petits)
+print("Plus grands:", plus_grands)
