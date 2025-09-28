f3c9a3ea0c9acbd0b51d835f2fb2bc5896665d0a
sirallahm-del <sirallahm@gmail.com>
2025-09-28 19:14:15 +0100

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,30 +1,15 @@
-# Mon premier Quick Sort simple
+# Quick Sort étape par étape
 # Auteur: Sirallah
 
-def quick_sort(liste):
-    # Si la liste a 0 ou 1 élément, elle est déjà triée
-    if len(liste) <= 1:
-        return liste
-    
-    # On prend l'élément du milieu comme pivot
-    pivot = liste[0]
-    
-    # On crée deux nouvelles listes
-    petits = []
-    grands = []
-    
-    # On met chaque élément dans la bonne liste
-    for element in liste[1:]:
-        if element < pivot:
-            petits.append(element)
-        else:
-            grands.append(element)
-    
-    # On combine tout
-    return quick_sort(petits) + [pivot] + quick_sort(grands)
+# Notre liste à trier
+liste = [5, 2, 8, 1, 9]
+print("Liste de départ:", liste)
+# On choisit le premier élément comme pivot
+pivot = liste[0]
+print("Notre pivot est:", pivot)
+# On crée deux listes vides
+plus_petits = []
+plus_grands = []
 
-# Test simple
-liste_test = [5, 2, 8, 1, 9]
-print("Avant:", liste_test)
-resultat = quick_sort(liste_test)
-print("Après:", resultat)
+print("Listes créées - plus_petits:", plus_petits)
+print("Listes créées - plus_grands:", plus_grands)
